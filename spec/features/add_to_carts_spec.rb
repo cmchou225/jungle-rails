63f20bb4657dfe35b1d1cb875a800cfce282e47a
Charles <cming.chou@gmail.com>
2017-05-16 21:23:20 +0000

    cart update testing completed

--- spec/features/add_to_carts_spec.rb
+++ spec/features/add_to_carts_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "User selects a product to show product details", type: :feature, js: true do
+RSpec.feature "User adds products to carts", type: :feature, js: true do
   before :each do
     @category = Category.create(name: 'Apparel')
 
@@ -15,11 +15,12 @@ RSpec.feature "User selects a product to show product details", type: :feature, 
     end
   end
 
-  scenario "User can see product details when selecting product" do
+  scenario 'User clicks add button on product and cart count updates' do
     visit root_path
-    first('article.product').click_link('Details')
-    sleep(5)
-    save_screenshot 'product_details.png'
-    expect(page).to have_css('article.product-detail')
+    first('article.product').click_link('Add')
+    sleep(4)
+    save_screenshot 'cart_count.png'
+
+    expect(page).to have_content("My Cart (1)")
   end
 end
